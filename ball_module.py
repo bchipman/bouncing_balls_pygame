6f8e9bacefa59e2a43c2cc2b05ebba4d2bd36fed
Brian <namedtuple@gmail.com>
2014-07-17 19:13:09 -0700

    Basic refactor; moved classes around, moved default arg order around, etc.

--- ball_module.py
+++ ball_module.py
@@ -2,30 +2,36 @@ import calculate
 import colors
 import random
 class Ball:
-    def __init__(self, number, center, radius, velocity, color):
-        self.identity = number
-        self.center = center
-        self.radius = radius
-        self.velocity = velocity
+    def __init__(self, number, color, center, radius, velocity, window_size):
+        self.window_size = window_size
+        self.number = number
         self.color = color
-        self.edges = calculate.edge_values(self.center, self.radius)
-    def move(self, window):
-        x,   y = self.center
-        dx, dy = self.velocity
-        self.center = (x + dx, y + dy)
-        self.edges = calculate.edge_values(self.center, self.radius)
-        self._change_direction_if_necessary(window)
-    def _change_direction_if_necessary(self, window):
-        if calculate.wall_collision(self, window):
-            wall_that_was_hit = calculate.wall_hit(self, window)
-            new_velocity = calculate.velocity_after_wall_collision_v0(self, wall_that_was_hit)
-            self.velocity = new_velocity
+        radius_Coordinate = Coordinate((radius, radius))  # calculate absolute version only once, at beginning (assuming window cannot be resized)
+        self.radius_absolute = radius_Coordinate.absolute(window_size)
+        velocity_Coordinate = Coordinate(velocity)          # calculate absolute version only once, at beginning (assuming window cannot be resized)
+        self.velocity_absolute = velocity_Coordinate.absolute(window_size)
+        center_Coordinate = Coordinate(center)            # calculate absolute version only once, at beginning (assuming window cannot be resized)
+        self.center_absolute = center_Coordinate.absolute(window_size)
+    def move(self):
+        x_abs, y_abs = self.center_absolute
+        dx_abs, dy_abs = self.velocity_absolute
+        new_x_abs = x_abs + dx_abs
+        new_y_abs = y_abs + dy_abs
+        new_xy_abs = new_x_abs, new_y_abs
+        self.center_absolute = new_xy_abs
+        self._change_velocity_direction_if_necessary(new_xy_abs)
+    def _change_velocity_direction_if_necessary(self, new_position_abs):
+        ball_edges = calculate.edge_values(new_position_abs, self.radius_absolute)
+        walls_hit = calculate.walls_hit(ball_edges, self.window_size)
+        new_velocity_abs = calculate.velocity_after_wall_collision(self.velocity_absolute, walls_hit)
+        self.velocity_absolute = new_velocity_abs
 class BallCreator:
     def __init__(self, options):
         self.number_balls = options.total_number_balls
         self.center_xy_range = options.center_xy_range
         self.radius_range = options.radius_range
         self.velocity_range = options.velocity_range
+        self.window_size = options.window_size
         self.balls = self._setup_balls()
     def _setup_balls(self):
         balls = []
@@ -37,13 +43,13 @@ class BallCreator:
         xy, XY = self.center_xy_range
         r, R = self.radius_range
         v, V = self.velocity_range
+        C = BallCreator.RND(colors.colors)
         X = BallCreator.RND(self.center_xy_range)
         Y = BallCreator.RND(self.center_xy_range)
         R = BallCreator.RND(self.radius_range)
         V = BallCreator.RND(self.velocity_range)
         V = BallCreator.RND([-V, V])
-        C = BallCreator.RND(colors.colors)
-        return Ball(number=N, center=(X, Y), radius=R, velocity=(V, V), color=C)
+        return Ball(number=N, color=C, center=(X, Y), radius=R, velocity=(V, V), window_size=self.window_size)
     @staticmethod
     def RND(range_or_options):
         if type(range_or_options) is tuple:
@@ -54,5 +60,19 @@ class BallCreator:
                 return random.uniform(a, b)
         elif type(range_or_options) is list:
             return random.choice(range_or_options)
-if __name__ == '__main__':
-    pass
+class Coordinate:
+    def __init__(self, xy, total_size=None):
+        x, y = xy
+        if type(x) == type(y) == int:       # Given in pixels (absolute)
+            W, H = total_size
+            self._relative_x = x / W
+            self._relative_y = y / H
+        elif type(x) == type(y) == float:   # Given in proportions (relative)
+            self._relative_x = x
+            self._relative_y = y
+    def relative(self):
+        return (self._relative_x, self._relative_y)
+    def absolute(self, absolute_size):
+        W, H = absolute_size
+        return (int(self._relative_x * W), int(self._relative_y * H))
+        return (int(self._relative_x * W), int(self._relative_y * H))
--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-from ball_module import BallCreator
+import ball_module
 from colors import *
-from collections import namedtuple
 import pygame
 import sys
-import time
 class Main:
     def __init__(self):
         self.options = Options()
         self.window = Window(self.options.window_size)
-        self.balls = BallCreator(self.options).balls
+        self.balls = ball_module.BallCreator(self.options).balls
         self.START_GAME_LOOP()
     def START_GAME_LOOP(self):
         while True:
             self.check_for_pygame_quit_event()
             self.window.surface.fill(BLACK)
             for ball in self.balls:
-                ball.move(self.window)
-                center_coordinate = Coordinate(ball.center)
-                radius_coordinate = Coordinate((ball.radius, ball.radius))
-                center_pixels = center_coordinate.absolute(self.window.size)
-                radius_pixels = radius_coordinate.absolute(self.window.size)[0]
-                pygame.draw.circle(self.window.surface, ball.color, center_pixels, radius_pixels)
-                pygame.draw.circle(self.window.surface, ball.color, center_pixels, radius_pixels)
+                ball.move()
+                center_in_pixels = ball.center_absolute
+                radius_in_pixels_tuple = ball.radius_absolute
+                radius_in_pixels = radius_in_pixels_tuple[0]
+                print('center_in_pixels', center_in_pixels)
+                print('radius_in_pixels', radius_in_pixels)
+                pygame.draw.circle(self.window.surface, ball.color, center_in_pixels, radius_in_pixels)
             pygame.display.update()
             pygame.time.delay(20)
     def check_for_pygame_quit_event(self):
@@ -35,29 +33,13 @@ class Window:
         self.width = size[0]
         self.height = size[1]
         self.surface = pygame.display.set_mode(size)
-        pygame.display.set_caption('Version 0')
+        pygame.display.set_caption('Version 1')
 class Options:
     def __init__(self):
-        self.total_number_balls = 3                 # integer
-        self.center_xy_range = (0.010, 0.99)     # proportion of window
-        self.radius_range = (0.025, 0.075)    # proportion of window
-        self.velocity_range = (0.005, 0.010)    # proportion of window
-        self.window_size = (600, 600)        # pixels
-class Coordinate:
-    def __init__(self, xy, total_size=None):
-        x, y = xy
-        if type(x) == type(y) == int:       # Given in pixels (absolute)
-            W, H = total_size
-            self._relative_x = x / W
-            self._relative_y = y / H
-        elif type(x) == type(y) == float:   # Given in proportions (relative)
-            self._relative_x = x
-            self._relative_y = y
-    def relative(self):
-        return (self._relative_x, self._relative_y)
-    def absolute(self, absolute_size):
-        W, H = absolute_size
-        return (int(self._relative_x * W), int(self._relative_y * H))
-        return (int(self._relative_x * W), int(self._relative_y * H))
+        self.total_number_balls = 3  # integer
+        self.center_xy_range = (0.010, 0.99)  # proportion of window
+        self.radius_range = (0.025, 0.075)  # proportion of window
+        self.velocity_range = (0.005, 0.010)  # proportion of window
+        self.window_size = (600, 600)  # pixels
 if __name__ == '__main__':
     Main()
