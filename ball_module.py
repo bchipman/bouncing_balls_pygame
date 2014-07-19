9801b5c73483a60a9c94893cfb26caac840f4b34
Brian <namedtuple@gmail.com>
2014-07-18 17:38:22 -0700

    Balls now display their number in the center (roughly).

--- ball_module.py
+++ ball_module.py
@@ -1,6 +1,8 @@
 import calculate
 import colors
 import random
+
+
 class Ball:
     def __init__(self, number, color, center, radius, velocity, window_size):
         self.window_size = window_size
@@ -12,6 +14,7 @@ class Ball:
         self.velocity_absolute = velocity_Coordinate.absolute(window_size)
         center_Coordinate = Coordinate(center)            # calculate absolute version only once, at beginning (assuming window cannot be resized)
         self.center_absolute = center_Coordinate.absolute(window_size)
+
     def move(self):
         x_abs, y_abs = self.center_absolute
         dx_abs, dy_abs = self.velocity_absolute
@@ -20,11 +23,14 @@ class Ball:
         new_xy_abs = new_x_abs, new_y_abs
         self.center_absolute = new_xy_abs
         self._change_velocity_direction_if_necessary(new_xy_abs)
+
     def _change_velocity_direction_if_necessary(self, new_position_abs):
         ball_edges = calculate.edge_values(new_position_abs, self.radius_absolute)
         walls_hit = calculate.walls_hit(ball_edges, self.window_size)
         new_velocity_abs = calculate.velocity_after_wall_collision(self.velocity_absolute, walls_hit)
         self.velocity_absolute = new_velocity_abs
+
+
 class BallCreator:
     def __init__(self, options):
         self.number_balls = options.total_number_balls
@@ -33,12 +39,14 @@ class BallCreator:
         self.velocity_range = options.velocity_range
         self.window_size = options.window_size
         self.balls = self._setup_balls()
+
     def _setup_balls(self):
         balls = []
         for n in range(0, self.number_balls):
             ball = self._create_random_ball(n)
             balls.append(ball)
         return balls
+
     def _create_random_ball(self, N):
         xy, XY = self.center_xy_range
         r, R = self.radius_range
@@ -50,6 +58,7 @@ class BallCreator:
         V = BallCreator.RND(self.velocity_range)
         V = BallCreator.RND([-V, V])
         return Ball(number=N, color=C, center=(X, Y), radius=R, velocity=(V, V), window_size=self.window_size)
+
     @staticmethod
     def RND(range_or_options):
         if type(range_or_options) is tuple:
@@ -60,6 +69,8 @@ class BallCreator:
                 return random.uniform(a, b)
         elif type(range_or_options) is list:
             return random.choice(range_or_options)
+
+
 class Coordinate:
     def __init__(self, xy, total_size=None):
         x, y = xy
@@ -70,8 +81,10 @@ class Coordinate:
         elif type(x) == type(y) == float:   # Given in proportions (relative)
             self._relative_x = x
             self._relative_y = y
+
     def relative(self):
         return (self._relative_x, self._relative_y)
+
     def absolute(self, absolute_size):
         W, H = absolute_size
         return (int(self._relative_x * W), int(self._relative_y * H))
--- main.py
+++ main.py
@@ -2,31 +2,41 @@ import ball_module
 from colors import *
 import pygame
 import sys
+
+
 class Main:
     def __init__(self):
         self.options = Options()
         self.window = Window(self.options.window_size)
         self.balls = ball_module.BallCreator(self.options).balls
         self.START_GAME_LOOP()
+
     def START_GAME_LOOP(self):
+        pygame.init()
         while True:
             self.check_for_pygame_quit_event()
             self.window.surface.fill(BLACK)
             for ball in self.balls:
                 ball.move()
                 center_in_pixels = ball.center_absolute
-                radius_in_pixels_tuple = ball.radius_absolute
-                radius_in_pixels = radius_in_pixels_tuple[0]
+                radius_in_pixels = ball.radius_absolute[0] # is a tuple; allows possibility for ovals later.
                 print('center_in_pixels', center_in_pixels)
                 print('radius_in_pixels', radius_in_pixels)
                 pygame.draw.circle(self.window.surface, ball.color, center_in_pixels, radius_in_pixels)
+                basic_font = pygame.font.SysFont(None, 24)
+                text = basic_font.render(str(ball.number), True, BLACK)
+                self.window.surface.blit(text, center_in_pixels)
+                
             pygame.display.update()
             pygame.time.delay(20)
+
     def check_for_pygame_quit_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+
 class Window:
     def __init__(self, size):
         self.size = size
@@ -34,12 +44,16 @@ class Window:
         self.height = size[1]
         self.surface = pygame.display.set_mode(size)
         pygame.display.set_caption('Version 1')
+
+
 class Options:
     def __init__(self):
-        self.total_number_balls = 3  # integer
+        self.total_number_balls = 10  # integer
         self.center_xy_range = (0.010, 0.99)  # proportion of window
         self.radius_range = (0.025, 0.075)  # proportion of window
         self.velocity_range = (0.005, 0.010)  # proportion of window
         self.window_size = (600, 600)  # pixels
+
+
 if __name__ == '__main__':
     Main()
