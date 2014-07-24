82e505cc42034d6050c765ae685b9f49b95389c5
Brian <namedtuple@gmail.com>
2014-07-23 18:12:54 -0700

    Code reorganization, added window position settings for desktop computer.

--- main.py
+++ main.py
@@ -7,11 +7,25 @@ import sys
 import pygame
 
 
+options = gfs.mk_namedtuple('Options', dict(
+    total_number_balls  = 10,                   # integer
+    center_xy_range     = (0.010,   0.99 ),     # proportion of window
+    radius_range        = (0.025,   0.075),     # proportion of window
+    velocity_range      = (0.005,   0.010),     # proportion of window
+    window_size         = (  300,   300  ),     # pixels
+    window_pos_desktop  = (-1000,   150  ),     # pixels
+    window_pos_laptop   = ( 1050,   150  ),     # pixels
+    ))    
+
+
 class Main:
     def __init__(self):
-        def _setup_pygame():
-            if os.environ['COMPUTERNAME'] == 'BRIAN-LAPTOP':
-                os.environ['SDL_VIDEO_WINDOW_POS'] = "1050, 150"  # positions window
+        def _setup_window():
+            if os.environ['COMPUTERNAME'] == 'BRIAN-DESKTOP':
+                os.environ['SDL_VIDEO_WINDOW_POS'] = '{},{}'.format(options.window_pos_desktop[0], options.window_pos_desktop[1])  # positions window
+
+            elif os.environ['COMPUTERNAME'] == 'BRIAN-LAPTOP':
+                os.environ['SDL_VIDEO_WINDOW_POS'] = '{},{}'.format(options.window_pos_laptop[0], options.window_pos_laptop[1])  # positions window
             pygame.init()
 
         def _setup_font():
@@ -19,15 +33,14 @@ class Main:
 
         def _setup_screen():
             display = pygame.display
-            screen  = display.set_mode(self.options.window_size)
+            screen  = display.set_mode(options.window_size)
             display.set_caption('Bouncing Balls!')
             return screen
         
-        _setup_pygame()
-        self.options = options
-        self.font    = _setup_font()
+        _setup_window()
         self.screen  = _setup_screen()
-        self.balls   = ball_module.BallCreator(self.options).balls
+        self.font    = _setup_font()
+        self.balls   = ball_module.BallCreator(options).balls
         self.START_GAME_LOOP()
 
     def START_GAME_LOOP(self):
@@ -55,14 +68,6 @@ class Main:
         while True:
             _check_for_pygame_quit_event()
             _redraw_screen()
-
-
-options = gfs.mk_namedtuple('Options', dict(
-    total_number_balls  = 10,                   # integer
-    center_xy_range     = (0.010,   0.99 ),     # proportion of window
-    radius_range        = (0.025,   0.075),     # proportion of window
-    velocity_range      = (0.005,   0.010),     # proportion of window
-    window_size         = (300,     300  )))    # pixels
 
 
 if __name__ == '__main__':
