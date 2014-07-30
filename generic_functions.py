5c517144ae626ea4a0204b38b88b73aa1547ba27
Brian <brian.chipman@gmail.com>
2014-07-30 09:40:06 -0700

    Removed the 'G' class and placed those attributes within 'Main' class namespace as 'self' attributes.  Some code organization as well and simplification of sys.path change.

--- generic_functions.py
+++ generic_functions.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 import sys
 import os
 import random
-
+#-------------------------------------------------------------------------------
 def mk_namedtuple(name, di):
     return namedtuple(name, di.keys())(**di)
 
@@ -11,7 +11,6 @@ def print_sys_path():
         print(repr(p))
 
 def change_sys_path():
-    # sys.path = sys.path[::-1]  # a simple list reversal seems to fix the import warning
     sys.path = sorted(sys.path) # simple list sorting also seems to be effective at removing the import warning
 
 def rnd(range_or_choices):
@@ -23,8 +22,11 @@ def rnd(range_or_choices):
             return random.uniform(a, b)
     elif type(range_or_choices) is list:
         return random.choice(range_or_choices)     
+#-------------------------------------------------------------------------------
+if __name__ != '__main__':
+    change_sys_path()
 
 if __name__ == '__main__':
-    change_sys_pathf()
+    change_sys_path()
     print_sys_path()
     import pygame
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 #!python3
-import generic_functions as gfs
-gfs.change_sys_path()
+import generic_functions as gfs  # first because of syspath change
 import ball_module
-from colors import *
 import os
 import sys
 import pygame
+from colors import *
 from pygame.locals import *
-
-
+#-------------------------------------------------------------------------------
 options = gfs.mk_namedtuple('Options', dict(
     total_number_balls  = 10,                   # integer
     center_xy_range     = (0.010,   0.99 ),     # proportion of window
@@ -18,12 +16,7 @@ options = gfs.mk_namedtuple('Options', dict(
     window_pos_desktop  = ( 2200,   150  ),     # pixels
     window_pos_laptop   = ( 1050,   150  ),     # pixels
     ))    
-
-
-class G:
-    pass
-
-
+#-------------------------------------------------------------------------------
 class Main:
     def __init__(self):
         def _setup_window():
@@ -37,83 +30,67 @@ class Main:
             pygame.mouse.set_visible(False)
             pygame.key.set_repeat(500, 100)
 
-        def _setup_font():
-            return pygame.font.SysFont(None, 24)
-
         def _setup_screen():
             display = pygame.display
             screen  = display.set_mode(options.window_size)
             display.set_caption('Bouncing Balls!')
             return screen
 
+        def _setup_font():
+            return pygame.font.SysFont(None, 24)
+
         _setup_window()
         _setup_mouse_keyboard()
-        G.flash         = False
-        G.pause         = False
-        G.adv_one_frame = False
-        G.screen        = _setup_screen()
-        G.font          = _setup_font()
-        G.balls         = ball_module.BallCreator(options).balls
-    #-------------------------------------------------------------------------------
-    def __call__(self):
-        while True:
-            self.handle_events()
-            if G.pause:
-                if G.adv_one_frame:
-                    self.move_balls()
-                    self.draw_screen()
-                    G.pause = True
-                    G.adv_one_frame = False
-                else:
-                    self.draw_screen()
-
-            elif not G.pause:
-                self.move_balls()
-                self.draw_screen()
+        self.flash         = False
+        self.pause         = False
+        self.adv_one_frame = False
+        self.screen        = _setup_screen()
+        self.font          = _setup_font()
+        self.balls         = ball_module.BallCreator(options).balls
     #-------------------------------------------------------------------------------
     def handle_events(self):
         def _check_for_quit_event():
-            for event in G.curr_events:
+            for event in self.curr_events:
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
 
         def _check_for_key_press_events():
-            for event in G.curr_events:
+            for event in self.curr_events:
                 if event.type == KEYDOWN:
                     if event.key == K_f:    
-                        G.flash = True
+                        self.flash = True
                     if event.key == K_RIGHT:
-                        G.adv_one_frame = True
+                        self.adv_one_frame = True
                 elif event.type == KEYUP:
                     if event.key == K_f:    
-                        G.flash = False
+                        self.flash = False
                     if event.key == K_p:
-                        if G.pause: 
-                            G.pause = False
-                        elif not G.pause: 
-                            G.pause = True
+                        if self.pause: 
+                            self.pause = False
+                        elif not self.pause: 
+                            self.pause = True
         
-        G.curr_events = pygame.event.get()
-        G.mouse_position = pygame.mouse.get_pos()
+        self.curr_events = pygame.event.get()
+        self.mouse_position = pygame.mouse.get_pos()
         _check_for_quit_event()
         _check_for_key_press_events()
     #-------------------------------------------------------------------------------
     def draw_screen(self):
         def _draw_screen():
-            if G.flash:
-                G.screen.fill(RED)
-            elif not G.flash:
-                G.screen.fill(BLACK)
+            if self.flash:
+                self.screen.fill(RED)
+            elif not self.flash:
+                self.screen.fill(BLACK)
         
         def _draw_balls():
-            for ball in G.balls:
-                pygame.draw.circle(G.screen, ball.color, ball.position, ball.radius)
-                G.screen.blit(ball.text_rendered, ball.text_position)
-            pygame.draw.circle(G.screen, RED, G.mouse_position, 3)
+            for ball in self.balls:
+                pygame.draw.circle(self.screen, ball.color, ball.position, ball.radius)
+                self.screen.blit(ball.text_rendered, ball.text_position)
+            pygame.draw.circle(self.screen, RED, self.mouse_position, 3)
 
         def _draw_mouse():
-            pygame.draw.circle(G.screen, RED, G.mouse_position, 3)
+            pygame.draw.circle(self.screen, RED, self.mouse_position, 3)
 
         def _update_screen():
             pygame.display.update()
@@ -125,7 +102,21 @@ class Main:
         _update_screen()
     #-------------------------------------------------------------------------------
     def move_balls(self):
-        G.balls = ball_module.BallHandler(G.balls, G.font)()
-
+        self.balls = ball_module.BallHandler(self.balls, self.font)()
+    #-------------------------------------------------------------------------------
+    def __call__(self):
+        while True:
+            self.handle_events()
+            if self.pause and self.adv_one_frame:
+                self.move_balls()
+                self.draw_screen()
+                self.pause = True
+                self.adv_one_frame = False
+            elif self.pause and not self.adv_one_frame:
+                self.draw_screen()
+            elif not self.pause:
+                self.move_balls()
+                self.draw_screen()
+    #-------------------------------------------------------------------------------
 if __name__ == '__main__':
     Main()()
